d6fd6c7da19b284337593029dd1158a700824c30
Nailton Mendes <jnmp25jnmp@gmail.com>
2022-04-25 17:57:49 -0300

    Ajuste na classe

---
+++ src/main/java/br/com/model/ProdutoPedido.java
@@ -0,0 +1,47 @@
+package br.com.model;
+
+public class ProdutoPedido {
+
+	private long id;
+	private Long idProduto;
+	private Long idPedido;
+
+	public ProdutoPedido() {
+
+	}
+
+	public ProdutoPedido(Long idProduto, Long idPedido) {
+		this.idProduto = idProduto;
+		this.idPedido = idPedido;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	@Override
+	public String toString() {
+		return "PordutoPedido [id=" + id + ", idProduto=" + idProduto + ", idPedido=" + idPedido + "]";
+	}
+
+}
